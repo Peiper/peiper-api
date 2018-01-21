8e837380a6cf0b673b54a2a31e0b161ff6c92480
peiper <niklas.peiper@gmail.com>
2018-01-21 11:31:12 +0100

    new project started as 2.0 with publiish

---
+++ PeiperApi/api/Controllers/NowController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class NowController : Controller
+    {
+        [HttpGet]
+        public BaseResponse<string> Now()
+        {
+            var value = "Värde från apiet";
+            return new BaseResponse<string>(value);
+        }
+
+        [HttpGet("add/{a}/{b}")]
+        public BaseResponse<int> Add(int a, int b)
+        {
+            var value = a + b;
+            return new BaseResponse<int>(value);
+        }
+    }
+}
---
+++ PeiperApi/api/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public class BaseResponse<T>
+    {
+        public T Result { get; set; }
+        public BaseResponse(T value)
+        {
+            Result = value;
+        }
+    }
+}
