04a06d2f3a242379d23db570ae1736377d0aa7e1
peiper <niklas.peiper@gmail.com>
2018-01-21 17:51:11 +0100

    set entity key. publish

--- PeiperApi/api/Repository/DbContext.cs
+++ PeiperApi/api/Repository/DbContext.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
-    public class Test
+    public class Test : CommonEntity
     {
         public string Var1 { get; set; }
         public string Var2 { get; set; }
     }
+
+    public class CommonEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+
     public class DbPsqlContext : DbContext
     {
         public DbSet<Test> TestData { get; set; }
