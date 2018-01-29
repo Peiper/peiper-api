bc485bc5ebbf98f94d6fc6f1fc3537a2200fe791
Niklas Peiper <niklas.peiper@gmail.com>
2018-01-29 14:46:34 +0100

    Update BuildData.cs
    added properties and constructor

--- PeiperApi/PeiperApi.Domain/Models/Deploy/BuildData.cs
+++ PeiperApi/PeiperApi.Domain/Models/Deploy/BuildData.cs
@@ -11,12 +11,21 @@ namespace PeiperApi.Domain.Models.Deploy
     public class BuildData : Entity
     {
         public string status { get; set; }
+        public string hash { get; set; }
+        public string message { get; set; }
 
         public BuildData()
         {
             id = 0;
             created = DateTime.Now;
             status = BuildStatus.STARTED.ToString();
+        }
+        
+        public BuildData(BuildData data)
+        {
+            this = BuildData();
+            hash = data.hash;
+            message = data.message;
         }
     }
 }
