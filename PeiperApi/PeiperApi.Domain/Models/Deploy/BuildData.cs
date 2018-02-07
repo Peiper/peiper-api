158460fd0953bad20b390b1be1031a771985a79a
peiper <niklas.peiper@gmail.com>
2018-02-07 16:03:20 +0100

    added version to builddata model

--- PeiperApi/PeiperApi.Domain/Models/Deploy/BuildData.cs
+++ PeiperApi/PeiperApi.Domain/Models/Deploy/BuildData.cs
@@ -14,24 +14,7 @@ namespace PeiperApi.Domain.Models.Deploy
         public string Status { get; set; }
         public string Hash { get; set; }
         public string Message { get; set; }
-
-        public BuildData()
-        {
-
-        }
-
-        public BuildData(string hash, string message)
-        {
-            BuildDataSetup();
-            Hash = hash;
-            Message = message;
-        }
-
-        public void BuildDataSetup()
-        {
-            Created = DateTime.Now;
-            Status = BuildStatus.QUEUED.ToString();
-        }
+        public string Version { get; set; }
     }
 
     public class SiteBuild : BuildData { }
